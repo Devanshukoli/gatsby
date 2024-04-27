fb27e4e692f832b6e41440b1e7454514d75444fe
Devanshukoli <kolidevanshu02@gmail.com>
2024-04-27 15:36:05 +0530

    [feat] Added Link Component in pages.

--- my-first-gatsby-website/src/pages/about.js
+++ my-first-gatsby-website/src/pages/about.js
@@ -1,12 +1,17 @@
 import * as React from 'react'
+import { Link } from 'gatsby'
 
 const AboutPage = () => {
     return (
         <>
             <h1>About Page</h1>
+            <Link to="/">Back to Home</Link>
+            {/* <a href='/'>Back to home</a> */}
             <p>This is About page, I am putting Everything information regarding specific things here.</p>
         </>
     )
 }
+
+export const Head = () => <title>About Me</title>
 
 export default AboutPage
