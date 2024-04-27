946d88606c7228dca1f669cbf6263db45a326241
Devanshukoli <kolidevanshu02@gmail.com>
2024-04-27 14:59:44 +0530

    [feat] Used pm i, Then Created *About* page.

---
+++ my-first-gatsby-website/src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+const AboutPage = () => {
+    return (
+        <>
+            <h1>About Page</h1>
+            <p>This is About page, I am putting Everything information regarding specific things here.</p>
+        </>
+    )
+}
+
+export default AboutPage
