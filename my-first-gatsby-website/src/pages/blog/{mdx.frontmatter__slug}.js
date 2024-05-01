80f7fc2e6c6b641fd812c61bc6a892d17569d563
Devanshukoli <kolidevanshu02@gmail.com>
2024-05-01 16:20:58 +0530

    [feat] Add hero image using GatsbyImage component.

--- my-first-gatsby-website/src/pages/blog/{mdx.frontmatter__slug}.js
+++ my-first-gatsby-website/src/pages/blog/{mdx.frontmatter__slug}.js
@@ -1,12 +1,19 @@
 import * as React from 'react'
 import { graphql } from 'gatsby'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import Layout from '../../component/layout'
 import Seo from '../../component/seo'
 
 const BlogPost = ({ data, children }) => {
+    const image = getImage(data.mdx.frontmatter.hero_image)
     return (
         <Layout pageTitle="Super Cool Blog Posts">
-            <p>My blog post contents will go here (eventually).</p>
+            <p>Posted: {data.mdx.frontmatter.date}</p>
+            <GatsbyImage
+                image={image}
+                alt={data.mdx.frontmatter.hero_image_alt}
+            />
+            {children}
         </Layout>
     )
 }
@@ -17,11 +24,19 @@ export const query = graphql`
             frontmatter {
                 title
                 date(formatString: "MMMM D, YYYY")
+                hero_image_alt
+                hero_image_credit_link
+                hero_image_credit_text
+                hero_image {
+                    childImageSharp {
+                        gatsbyImageData
+                    }
+                }
             }
         }
     }
 `
 
-export const Head = () => <Seo title="Super Cool Blog Posts" />
+export const Head = ({data}) => <Seo title={data.mdx.frontmatter.title} />
 
 export default BlogPost
