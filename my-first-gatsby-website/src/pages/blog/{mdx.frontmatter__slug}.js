83270329bfd6dd081f3c3e6b72249364929f29e3
Devanshukoli <kolidevanshu02@gmail.com>
2024-05-01 14:21:08 +0530

    [feat] render post contents in the blog post page template.

--- my-first-gatsby-website/src/pages/blog/{mdx.frontmatter__slug}.js
+++ my-first-gatsby-website/src/pages/blog/{mdx.frontmatter__slug}.js
@@ -1,14 +1,26 @@
 import * as React from 'react'
+import { graphql } from 'gatsby'
 import Layout from '../../component/layout'
 import Seo from '../../component/seo'
 
-const BlogPost = () => {
+const BlogPost = ({ data, children }) => {
     return (
         <Layout pageTitle="Super Cool Blog Posts">
             <p>My blog post contents will go here (eventually).</p>
         </Layout>
     )
 }
+
+export const query = graphql`
+    query ($id: String) {
+        mdx(id: {eq: $id}) {
+            frontmatter {
+                title
+                date(formatString: "MMMM D, YYYY")
+            }
+        }
+    }
+`
 
 export const Head = () => <Seo title="Super Cool Blog Posts" />
 
